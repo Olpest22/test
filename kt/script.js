8cf9f919fceedb03e3822b958772d038f718bf34
Olpest22 <150504553+Olpest22@users.noreply.github.com>
2023-12-23 13:45:38 +1000

    kt
    kt

---
+++ kt/script.js
@@ -0,0 +1,12 @@
+function conteiner11(){
+ document.getElementById(1);
+}
+function conteiner22(){
+  document.getElementById("2");
+ }
+ function conteiner33(){
+  document.getElementById(3);
+ }
+ function conteiner44(){
+  document.getElementById("4");
+ }
